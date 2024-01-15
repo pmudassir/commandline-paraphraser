197b45bc4eb0ef6d144d89e48471138d35dc6629
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2024-01-15 14:37:31 +0530

    options added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const readline = require('readline');
-const OpenAI = require('openai');
+import readline from 'readline';
+import { OpenAI } from 'openai';
+import inquirer from 'inquirer';
+import dotenv from 'dotenv';
 
-require('dotenv').config()
+dotenv.config();
 
 const openai = new OpenAI({
     apiKey: process.env.API_KEY,
@@ -12,15 +14,44 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Enter the text to paraphrase: ', async (text) => {
-    rl.question('Choose option (Professional, Creative, Urgent): ', async (option) => {
-        const completion = await openai.chat.completions.create({
-            messages: [{ role: "user", content: `Paraphrase the given sentence : ${text} in a ${option} way or tone` }],
-            max_tokens: 150,
-            model: "gpt-3.5-turbo",
-        })
-        const paraphrasedText = completion.choices[0].message.content;
-        console.log('Paraphrased text:', paraphrasedText);
-        rl.close();
+function paraphrase() {
+    const styleOptions = ['Professional', 'Creative', 'Urgent'];
+
+    rl.question('Enter the text to paraphrase: ', async (text) => {
+        if (!text) {
+            rl.close();
+            return console.log("Please enter the text to paraphrase");
+        }
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'style',
+                message: 'Choose paraphrasing style:',
+                choices: styleOptions
+            }
+        ])
+            .then(async answers => {
+                const chosenStyle = answers.style;
+
+                try {
+                    const completion = await openai.chat.completions.create({
+                        messages: [{ role: "user", content: `Paraphrase the given sentence : ${text} in a ${chosenStyle} way or tone` }],
+                        max_tokens: 150,
+                        model: "gpt-3.5-turbo",
+                    });
+                    const paraphrasedText = completion.choices[0].message.content;
+                    console.log('Paraphrased text:', paraphrasedText);
+                } catch (error) {
+                    console.error('Error:', error);
+                } finally {
+                    rl.close();
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                rl.close();
+            });
     });
-});
+}
+
+paraphrase();
